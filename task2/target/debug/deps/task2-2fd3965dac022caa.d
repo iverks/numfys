54b9ad580caf1b3313fdde24aaeaacb109f07f1f
Iver Småge men bærbar <iversmage@gmail.com>
2023-02-12 16:46:35 +0100

    move to subfolder of numfys folder

---
+++ task2/target/debug/deps/task2-2fd3965dac022caa.d
@@ -0,0 +1,5 @@
+c:\Users\ivism\Progging\numfys\task2\target\debug\deps\task2-2fd3965dac022caa.rmeta: src\main.rs
+
+c:\Users\ivism\Progging\numfys\task2\target\debug\deps\task2-2fd3965dac022caa.d: src\main.rs
+
+src\main.rs:
